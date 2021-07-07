0be519390ade18b5ac8a22f6487170a8c6896faa
Dave Clayton <davedx@gmail.com>
2021-07-07 14:28:09 +0200

    Add comment

--- controller.c
+++ controller.c
@@ -6,6 +6,11 @@
 const float MIN_ACCELERATION = -3.f;
 const float MAX_ACCELERATION = 3.f;
 
+/**
+ * Naive controller. Jams the "stick" in whatever direction takes it towards
+ * the target at max, only reducing accleration when it's right on top of the
+ * target. Like how a 5 year old plays Mario Kart.
+ */
 Vec2 getAcceleration(SimState *simState) {
   float dx = simState->target.x - simState->vehicle.position.x;
   float dy = simState->target.y - simState->vehicle.position.y;
